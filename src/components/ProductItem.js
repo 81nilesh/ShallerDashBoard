b65f951a924fd9560ef2d9da95243305aa2310ea
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-12 15:12:35 +0530

    commit 6

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,58 +1,58 @@
 import React, { useState } from 'react';
 
 const ProductItem = ({ index, product, onDelete, onEdit }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedProduct, setEditedProduct] = useState(product);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedProduct, setEditedProduct] = useState(product);
 
-  const handleDelete = () => {
-    onDelete(index);
-  };
+    const handleDelete = () => {
+        onDelete(index);
+    };
 
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
 
-  const handleSave = () => {
-    onEdit(index, editedProduct);
-    setIsEditing(false);
-  };
+    const handleSave = () => {
+        onEdit(index, editedProduct);
+        setIsEditing(false);
+    };
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setEditedProduct({
-      ...editedProduct,
-      [name]: value,
-    });
-  };
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setEditedProduct({
+            ...editedProduct,
+            [name]: value,
+        });
+    };
 
-  return (
-    <div>
-      {isEditing ? (
+    return (
         <div>
-          <input
-            type="text"
-            name="name"
-            value={editedProduct.name}
-            onChange={handleInputChange}
-          />
-          <input
-            type="number"
-            name="price"
-            value={editedProduct.price}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleSave}>Save</button>
+            {isEditing ? (
+                <div>
+                    <input
+                        type="text"
+                        name="name"
+                        value={editedProduct.name}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="number"
+                        name="price"
+                        value={editedProduct.price}
+                        onChange={handleInputChange}
+                    />
+                    <button onClick={handleSave}>Save</button>
+                </div>
+            ) : (
+                <div>
+                    <span>Name: {product.name}</span>
+                    <span>Price: {product.price}</span>
+                    <button onClick={handleDelete}>Delete</button>
+                    <button onClick={handleEdit}>Edit</button>
+                </div>
+            )}
         </div>
-      ) : (
-        <div>
-          <span>Name: {product.name}</span>
-          <span>Price: {product.price}</span>
-          <button onClick={handleDelete}>Delete</button>
-          <button onClick={handleEdit}>Edit</button>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default ProductItem;
