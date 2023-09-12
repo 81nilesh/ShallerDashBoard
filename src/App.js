35e46e222e3f359858766e1eee5871a5054a5418
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-12 14:29:55 +0530

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
-// import ProductForm from './components/ProductForm';
-// import ProductItem from './components/ProductItem';
+import ProductForm from './components/ProductForm';
+import ProductItem from './components/ProductItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
-      {/* <ProductForm /> */}
-      {/* <ProductItem /> */}
+      <ProductForm />
+      <ProductItem />
 
     </div>
   );
